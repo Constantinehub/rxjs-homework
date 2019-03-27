0c58a4da584de58029bd921194d6d296e8803971
Constantine Shibaev <killtobanana@gmail.com>
2019-03-27 22:46:30 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,103 @@
-import { from, fromEvent, Observable, of, combineLatest, interval, timer} from 'rxjs';
-import { map, catchError, debounceTime, switchMap, delay, find, scan, filter, pluck, skip, startWith, take, takeLast, throttle} from 'rxjs/operators';
-
-const inputElement: HTMLInputElement = document.querySelector('#refInput') as HTMLInputElement;
-
-
-type IRepo =  { name: string, owner: { repos_url: string } }
-type IGithubRepos = { items: IRepo[] }
-
-
-const source1$: Observable<any> = fromEvent(inputElement, 'click').pipe(
-  throttle(x => interval(3000))
-);
-
-source1$.subscribe(data => console.log('double click'))
-
-
-
-
-const source$: Observable<IGithubRepos> = fromEvent(inputElement, 'input').pipe(
-  map((event: Event) => (event.target as any).value),
-  debounceTime(500),
-  switchMap((value: string) => {
-    console.log(value)
-    return from(
-      fetch(`https://api.github.com/search/repositories?q=${value}`)
-      .then((res: Response) => res.json())
-      .catch((err) => console.log(err))
-    )
-  }),
-  catchError((err: Error, caught: any) => {
-    return of(err)
-  })
-);
-
-source$.subscribe((repo: IGithubRepos) => {
-  const wrapperElement: HTMLInputElement = document.querySelector('.wrapper') as HTMLInputElement;
-  wrapperElement.innerHTML = '';
-
-  repo.items.forEach((item: IRepo) => {
-    const a: HTMLAnchorElement = document.createElement('a');
-    a.innerHTML = item.name;
-    a.setAttribute('href', item.owner.repos_url)
-    wrapperElement.appendChild(a)
-  })
-},
-  (err: Error) => {
-    console.log(err)
-  }
-)
-
-const a$: Observable<number> = of(1, 2).pipe(
-  delay(2000)
-);
-const b$: Observable<number> = of(2);
-const d$: Observable<number> = of(4);
-
-const array$ = [ a$, b$, d$,];
-
-
-const c$: Observable<number> = combineLatest(
-  ...array$,
-  (a: number, b: number, d: number) => a + b + d
-)
-
-c$.subscribe((c: number) => console.log(c))
-
-
-// interval(1000).pipe(
-//   map(() => new Date())
-// ).subscribe((date: Date) => {
-//   console.log(date)
-// })
-
-
-
-const id: number = 1;
-const item$: Observable<number> = from([1, 2, 3, 4])
-
-
-item$.pipe(
-  find((value: number, index: number) => value === id )
-).subscribe(data => console.log(data))
-
-
-item$.pipe(
-  takeLast(1),
-  startWith(20),
-  scan((acc: number, next: number) => acc + next, 0),
-  filter((value: number) => value > 5 ),
-  skip(1),
-  map((data: number) => ({ result: data })),
-  pluck('result'),
-).subscribe(data => console.log('sum', data))
-
-
+// import { from, fromEvent, Observable, of, combineLatest, interval, timer} from 'rxjs';
+// import { map, catchError, debounceTime, switchMap, delay, find, scan, filter, pluck, skip, startWith, take, takeLast, throttle} from 'rxjs/operators';
+//
+// const inputElement: HTMLInputElement = document.querySelector('#refInput') as HTMLInputElement;
+//
+//
+// type IRepo =  { name: string, owner: { repos_url: string } }
+// type IGithubRepos = { items: IRepo[] }
+//
+//
+// const source1$: Observable<any> = fromEvent(inputElement, 'click').pipe(
+//   throttle(x => interval(3000))
+// );
+//
+// source1$.subscribe(data => console.log('double click'))
+//
+//
+//
+//
+// const source$: Observable<IGithubRepos> = fromEvent(inputElement, 'input').pipe(
+//   map((event: Event) => (event.target as any).value),
+//   debounceTime(500),
+//   switchMap((value: string) => {
+//     console.log(value)
+//     return from(
+//       fetch(`https://api.github.com/search/repositories?q=${value}`)
+//       .then((res: Response) => res.json())
+//       .catch((err) => console.log(err))
+//     )
+//   }),
+//   catchError((err: Error, caught: any) => {
+//     return of(err)
+//   })
+// );
+//
+// source$.subscribe((repo: IGithubRepos) => {
+//   const wrapperElement: HTMLInputElement = document.querySelector('.wrapper') as HTMLInputElement;
+//   wrapperElement.innerHTML = '';
+//
+//   repo.items.forEach((item: IRepo) => {
+//     const a: HTMLAnchorElement = document.createElement('a');
+//     a.innerHTML = item.name;
+//     a.setAttribute('href', item.owner.repos_url)
+//     wrapperElement.appendChild(a)
+//   })
+// },
+//   (err: Error) => {
+//     console.log(err)
+//   }
+// )
+//
+// const a$: Observable<number> = of(1, 2).pipe(
+//   delay(2000)
+// );
+// const b$: Observable<number> = of(2);
+// const d$: Observable<number> = of(4);
+//
+// const array$ = [ a$, b$, d$,];
+//
+//
+// const c$: Observable<number> = combineLatest(
+//   ...array$,
+//   (a: number, b: number, d: number) => a + b + d
+// )
+//
+// c$.subscribe((c: number) => console.log(c))
+//
+//
+// // interval(1000).pipe(
+// //   map(() => new Date())
+// // ).subscribe((date: Date) => {
+// //   console.log(date)
+// // })
+//
+//
+//
+// const id: number = 1;
+// const item$: Observable<number> = from([1, 2, 3, 4])
+//
+//
+// item$.pipe(
+//   find((value: number, index: number) => value === id )
+// ).subscribe(data => console.log(data))
+//
+//
+// item$.pipe(
+//   takeLast(1),
+//   startWith(20),
+//   scan((acc: number, next: number) => acc + next, 0),
+//   filter((value: number) => value > 5 ),
+//   skip(1),
+//   map((data: number) => ({ result: data })),
+//   pluck('result'),
+// ).subscribe(data => console.log('sum', data))
 
 
 
-console.log('Common way', new Date())
+
+//
+// console.log('Common way', new Date())
 // const c: Observable<number> = a$.pipe(
 //   map((value: number) => )
 // )
